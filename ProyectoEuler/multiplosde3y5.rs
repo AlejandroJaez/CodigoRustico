639c6ebcfe175844654116ec9d1cfa09437972da
Alejandro Hernandez <alejandrojaez@outlook.com>
2021-02-18 19:40:44 -0600

    first commit

---
+++ ProyectoEuler/multiplosde3y5.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut sum = 0;
+    for number in 1..1000{
+        if number%3 == 0 {
+            println!("{}",number);
+            sum = sum + number;
+        } else if number%5 == 0 {
+            println!("{}",number);
+            sum = sum + number;
+        }
+    }
+
+    // Print text to the console
+    println!("{}",sum);
+}
