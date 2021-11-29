3ff300e96d33d03bbd202d7e4eae20149b6f9113
Alejandro Hernandez <alejandrojaez@outlook.com>
2021-11-29 11:03:08 -0600

    upd

--- ProyectoEuler/01_Multiplos_de_3_y_5.rs
+++ ProyectoEuler/01_Multiplos_de_3_y_5.rs
@@ -1,11 +1,16 @@
+use std::env;
+
+
 fn main() {
-    let mut sum = 0;
-    for number in 1..1000 {
+    let args: Vec<String> = env::args().collect();
+    let dest = &args[1];
+    let mut sum: u128 = 0;
+    for number in 1..dest.parse::<u128>().unwrap() {
         if number%3 == 0 {
-            println!("{}",number);
+            //println!("{}",number);
             sum = sum + number;
         } else if number%5 == 0 {
-            println!("{}",number);
+            //println!("{}",number);
             sum = sum + number;
         }
     }
